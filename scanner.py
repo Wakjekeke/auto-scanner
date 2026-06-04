3ad6bee8abc6d9b1b86d89675289379078c23d03
Wakjekeke <baro.ak47@icloud.com>
2026-06-05 01:11:58 +0200

    Update scanner.py

--- scanner.py
+++ scanner.py
@@ -1,1 +1,16 @@
-print("Scanner läuft")
+import json
+
+autos = [
+    {
+        "title": "BMW E46 320i",
+        "price": "2500 €",
+        "km": "197000 km",
+        "distance": "18 km entfernt",
+        "url": "https://www.kleinanzeigen.de/s-autos/sinzig/c216l5395"
+    }
+]
+
+with open("autos.json", "w", encoding="utf-8") as f:
+    json.dump(autos, f, ensure_ascii=False, indent=2)
+
+print("Autos gespeichert")
